5c1433fc22cc23702f109b515d383b5fb5e4ceb3
Deborah Lan <shlan@ucsd.edu>
2016-02-11 20:18:42 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -26,7 +26,25 @@ function addProjectDetails(e) {
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+	var websiteURL = "/project/" + idNumber
+
+	console.log("User clicked on " + websiteURL);
+
+	console.log(websiteURL)
+
+	$.get(websiteURL, getURL);
+
+}
+ 
+function getURL(result){
+	console.log(result);
+	var projectdetail = "#project" + result.id + " .details";
+
+	var projectHTML= '<a href="#">' +
+	'<img src=' + result['image'] + ' class="detailsImage">' +
+	'<p><small>' + result['date'] + '</small></p></a>' +
+	'<p><small>' + result['summary'] + '</small></p></a>';
+	$(projectdetail).html(projectHTML);
 }
 
 /*
@@ -35,4 +53,30 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	var paletteURL = "/palette";
+	$.get(paletteURL, getpaletteURL);
+
 }
+
+function getpaletteURL(colorResult){
+	console.log(colorResult);
+
+	var colorPalette = "#palette" + colorResult.id + " .colors"; 
+
+	$(colorPalette).html(colorResult);
+
+	var colors = colorResult.colors.hex;
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+
+
+
+
+
